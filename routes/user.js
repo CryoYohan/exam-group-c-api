d2f7d7fdca2bec4bdb2f0da286519d7700496763
unknown <YY447@ccs.local>
2025-04-04 04:03:19 -0700

    Creating post for creating exam

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/createexam', (req, res) => {
+    const examData = req.body; 
+    res.status(201).json({ message: 'Exam created successfully', data: examData });
+});
+
 router.get('/', (req, res) => {
     res.send('Welcome to the route!');
 });
