ed6c35e3fd3f7abb77c40433c8bbbe0bbc74d81b
unknown <YY447@ccs.local>
2025-04-04 04:04:39 -0700

    Modifying the creating exam route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/createexam', (req, res) => {
+router.post('/exams', (req, res) => {
     const examData = req.body; 
     res.status(201).json({ message: 'Exam created successfully', data: examData });
 });
+
+rou
 
 router.get('/', (req, res) => {
     res.send('Welcome to the route!');
